a1c0ce6b1eae8d974ab6a848b30e3e9c26c7395c
github username <winter.lamon@gmail.com>
2018-01-11 09:12:49 -0500

    config nested route (/api/v1/)

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
      namespace :v1 do
        resources :list_items, :list, :household, :users
-       # post '/auth', to: 'auth#create'
-       # get '/current_user', to: 'auth#show'
      end
    end
 end
