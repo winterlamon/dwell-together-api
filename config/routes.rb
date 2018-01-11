b9c21819799beabd98aa5d61a459bf8645f739ea
github username <winter.lamon@gmail.com>
2018-01-11 13:53:36 -0500

    updated config/routes.rb with auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
      namespace :v1 do
        resources :list_items, :list, :household, :users
+
+       post '/auth', to: 'auth#create'
+       get  '/current_user', to: 'auth#show'
      end
    end
 end
