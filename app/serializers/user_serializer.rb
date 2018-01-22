a2fa19c7e909f4644979ff966d518d8c09ff0ba7
github username <winter.lamon@gmail.com>
2018-01-21 20:07:01 -0500

    added description and avatar to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :email, :household_id
+  attributes :id, :first_name, :last_name, :username, :email, :household_id, :description, :avatar_url
 
   belongs_to :household
   has_many :list_items
